750b137cd9feae6ffeeb539917763b809f400619
Rafael Augusto <movcode@gmail.com>
2020-04-10 01:52:35 -0300

    remove mid in image route

--- src/api/images/images_router.js
+++ src/api/images/images_router.js
@@ -3,7 +3,7 @@ const action = require('./images_action')
 const route = "/image/upload"
 
 module.exports = router => middleware => {
-    router.post(route, middleware, singileUpload('file'), action.upload)
+    router.post(route, singileUpload('file'), action.upload)
 
     return router
 }
