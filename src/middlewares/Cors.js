507f53d5c0932ffbab726b15a31a2a36a614fc34
Rafael Augusto <movcode@gmail.com>
2020-04-10 02:42:51 -0300

    config cors

--- src/middlewares/Cors.js
+++ src/middlewares/Cors.js
@@ -1,8 +1,7 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');    
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.header("Access-Control-Allow-Headers",
+    res.setHeader('Access-Control-Allow-Origin', '*');    
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+    res.setHeader("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Authorization, Accept, Content-Type");
         res.setHeader('Access-Control-Allow-Credentials', true);
     return next();
